416c61855849ac8a63afb2af68cef2fd1fe4f48f
Musfiqur Rahman Zihad <musfiqurahmanzihad@gmail.com>
2024-03-02 20:00:54 +0600

    made the layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.css'
 
-function App() {
+const messages = [
+  'Learn React ⚛️',
+  'Apply for jobs 💼',
+  'Invest your new income 🤑',
+];
+
+const App = () => {
+  const step = 1;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
+        <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
+        <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
+      </div>
+
+      <p class="message">Step {step}: {messages[step - 1]}</p>
+
+      <div class="buttons">
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }} class="previous">Previous</button>
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }} class="previous">Next</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
